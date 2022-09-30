dac52a47b99166b4ebc5b1ef4e566c8c4207409f
patience1000 <paulpatience577@gmail.com>
2022-09-30 11:08:32 +0100

    modified:   Script.js 	modified:   about.htm 	modified:   style.css

--- Script.js
+++ Script.js
@@ -8,4 +8,13 @@ function myFunction() {
 }
 
 
+function Show1() {
+  document.getElementById('div2').style.display = "block";
+  document.getElementById('div1').style.display = "none";
+}
+
+function Show2() {
+  document.getElementById('div1').style.display = "block";
+  document.getElementById('div2').style.display = "none";
+}
 
