9bde0194ab5f56a17607b4dcde719e4e3464d4e3
patience1000 <paulpatience577@gmail.com>
2022-10-15 19:19:07 +0100

    Updated search bar

--- Script.js
+++ Script.js
@@ -7,7 +7,17 @@ function myFunction() {
   }
 }
 
+function openPage(){
+  var y = document.getElementById("search").value;
 
+  if (y == "About"){
+    window.open("about.html");
+  }
+
+  if(y == "sale"){
+    window.location.href="#search";
+  }
+}
 function Show1() {
   document.getElementById('div2').style.display = "block";
   document.getElementById('div1').style.display = "none";
