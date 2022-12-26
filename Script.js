e2ef4ed168213c15d0b7c66447bbae9f6e7310c4
patience1000 <paulpatience577@gmail.com>
2022-12-26 21:19:03 +0100

    working on the toggle button

--- Script.js
+++ Script.js
@@ -33,7 +33,17 @@ function Show2() {
   document.getElementById('div2').style.display = "none";
 }
 
-function showCart(){
-  document.getElementById('cart').style.display = "block";
-  document.getElementById('cart').style.display = "none";
+// function showCart(){
+//   document.getElementById('cart').style.display = "block";
+//   document.getElementById('cart').style.display = "none";
+// }
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
+
